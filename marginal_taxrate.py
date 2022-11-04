86642a684f63014d5120018295ac6e11caeb0764
chonlameth <chonlameth@sit.kmutt.ac.th>
2022-11-04 11:08:50 +0700

    Add files via upload

---
+++ marginal_taxrate.py
@@ -0,0 +1,8 @@
+def marginal_tax_rate(income):
+    if income < 10000:
+        return 0.0
+    elif income < 20000:
+        return 5.0
+    else
+        return 7.0
+        
---
+++ taxrate.py
@@ -0,0 +1,6 @@
+def tax_rate(income):
+    if income < 10000:
+        return 0.0
+    else:
+        return 5.0
+        
